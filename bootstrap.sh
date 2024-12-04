c0df6e667940d42e0cc16a75321a52510759afef
xiatiandeairen <384660495@qq.com>
2024-12-05 01:11:25 +0800

    feat: add broot dot-config
    https://dystroy.org/broot/conf_file/#reset
    
    close #9

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ function apply() {
 }
 
 function bootstrap() {
-    targets=(zsh brew bat)
+    targets=(zsh brew bat broot)
     for target in "${targets[@]}"; do
         apply ${target}
     done

c4655c7bcdebf9f39a892c79243334cfb59120e9
xiatiandeairen <384660495@qq.com>
2024-12-05 00:14:31 +0800

    feat: add install and bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#! /bin/bash -x
+
+function apply() {
+    local target="$1"
+    $(which stow) -t ${HOME} ${target} --dotfiles --adopt --verbose -d ${DOTFILES}
+}
+
+function bootstrap() {
+    targets=(zsh)
+    for target in "${targets[@]}"; do
+        apply ${target}
+    done
+}
+
+function dot() {
+    if [ $# -eq 0 ]; then
+        echo "Usage: dot [pull|push|status|apply [path]]|bootstrap"
+        return 1
+    fi
+
+    local command="$1"
+    local target="$2"
+
+    case "$command" in
+        "pull")
+            $(which git) -C "{$DOTFILES}" pull origin main --autostash
+        ;;
+        
+        "push")
+            $(which git) -C "${DOTFILES}" push origin main
+        ;;
+        
+        "status")
+            $(which git) -C "${DOTFILES}" status
+        ;;
+        
+        "apply")
+            apply ${target}
+        ;;
+        
+        "bootstrap")
+            bootstrap
+        ;;
+
+    esac
+}
+
+

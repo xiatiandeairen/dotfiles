873f5d956f1e7791f16e99bb492cc0ec9174dc8f
xiatiandeairen <384660495@qq.com>
2024-12-08 22:05:12 +0800

    feat: support benchmark command and improve help message

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,7 +39,37 @@ function bootstrap() {
 
 function dot() {
     if [ $# -eq 0 ]; then
-        echo "Usage: dot [pull|push|commit|diff|cd|status|apply [path]]|bootstrap"
+        echo "
+            Usage: dotfiles <command> [target]
+
+            Commands:
+            pull        Fetch the latest changes from the remote repository and merge them into the target branch (default: develop).
+                        Usage: dotfiles pull [target]
+            
+            push        Push your local changes to the remote repository on the target branch (default: develop).
+                        Usage: dotfiles push [target]
+            
+            commit      Stage all changes and commit them with a default message.
+                        Usage: dotfiles commit
+            
+            diff        Show the differences between your local changes and the latest commit.
+                        Usage: dotfiles diff
+            
+            cd          Change directory to the dotfiles repository.
+                        Usage: dotfiles cd
+            
+            status      Show the current status of your dotfiles repository.
+                        Usage: dotfiles status
+            
+            apply       Apply a patch or configuration.
+                        Usage: dotfiles apply [target]
+            
+            bootstrap   Initialize or set up the dotfiles (e.g., by installing dependencies).
+                        Usage: dotfiles bootstrap
+            
+            benchmark   Run benchmark tests on the dotfiles setup.
+                        Usage: dotfiles benchmark
+        "
         return 1
     fi
 
@@ -77,6 +107,10 @@ function dot() {
         
         "bootstrap")
             bootstrap
+        ;;
+
+        "benchmark")
+            zsh ${ZDOTDIR}/plugins/zsh-bench/zsh-bench
         ;;
 
     esac

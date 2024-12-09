4baacfe38f4249258e21ad9b3d03f03e1a2caf8b
xiatiandeairen <384660495@qq.com>
2024-12-09 23:58:05 +0800

    fix: supplement brew bin path

--- zsh/dot-config/zsh/path.zsh
+++ zsh/dot-config/zsh/path.zsh
@@ -1,2 +1,2 @@
 [ -d ${HOME}/.local/bin ] && export PATH="${HOME}/.local/bin:${PATH}"
-[ -d /opt/homebrew/bin ] && export PATH="/opt/homebrew/bin:$PATH"
+[ -d /opt/homebrew/bin ] && export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}"

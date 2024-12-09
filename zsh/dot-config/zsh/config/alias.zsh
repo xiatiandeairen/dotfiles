ffb74c102a687a483ce8a0d6f7f714f0273370aa
xiatiandeairen <384660495@qq.com>
2024-12-09 23:08:20 +0800

    feat: add alias.zsh

---
+++ zsh/dot-config/zsh/config/alias.zsh
@@ -0,0 +1,12 @@
+# bat
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
+# broot
+alias br=broot
+
+# tmux
+alias tmux="tmux -f ${TMUX_CONFIG_PATH}"
+
+# custom
+alias delete-dsstore='find . -name ".DS_Store" -exec rm -f {} \;'

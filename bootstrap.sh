bfa71864063cf1a0ed320526ad80ffa7e1be5147
xiatiandeairen <384660495@qq.com>
2024-12-11 00:18:26 +0800

    feat: add nvim dot-config

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ function apply {
 }
 
 function bootstrap {
-    targets=(zsh brew bat broot eza tig tmux zoxide vim thefuck)
+    targets=(zsh brew bat broot eza tig tmux zoxide vim thefuck nvim)
     for target in "${targets[@]}"; do
         apply ${target}
     done

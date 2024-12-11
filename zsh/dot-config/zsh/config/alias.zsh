11cf1d6e04e6e74e535616f5913af7be876b48ab
xiatiandeairen <384660495@qq.com>
2024-12-11 21:38:28 +0800

    feat: add eza alias named e, el and et
    close #46

--- zsh/dot-config/zsh/config/alias.zsh
+++ zsh/dot-config/zsh/config/alias.zsh
@@ -8,6 +8,13 @@ alias br=broot
 # tmux
 alias tmux="tmux -f ${TMUX_CONFIG_PATH}"
 
+# eza
+local default_opts="--git --icons --group-directories-first --sort=name --header"
+# set aliases using options from env var if available
+alias e='eza '"${EZA_CMD_OPTS:-$default_opts}" # base implementation
+alias el='e -lh --no-user' # mid form -user/group/hidden
+alias et='el --tree -L 3' # mid form tree listing
+
 # custom
 alias delete-dsstore='find . -name ".DS_Store" -exec rm -f {} \;'
 alias d='dirs -v'

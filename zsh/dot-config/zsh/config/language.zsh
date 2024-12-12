690e885dc90badfaf0a8d9fa0e3299c23d991d00
xiatiandeairen <384660495@qq.com>
2024-12-12 09:23:41 +0800

    feat: add few language config environment variable
    close #45 #20

--- zsh/dot-config/zsh/config/language.zsh
+++ zsh/dot-config/zsh/config/language.zsh
@@ -8,3 +8,24 @@ export npm_config_cache="${XDG_CACHE_HOME}/npm"
 export npm_config_prefix="${NPM_HOME}/npm-global"
 export PNPM_CONFIG_HOME="${PNPM_HOME}/config"
 export PNPM_STORE_PATH="${XDG_CACHE_HOME}/pnpm"
+
+# xmake
+export XMAKE_GLOBALDIR="${XDG_CONFIG_HOME}/xmake"
+export XMAKE_PKG_INSTALLDIR="${XMAKE_GLOBALDIR}/pkg"
+export XMAKE_PKG_CACHEDIR="${XDG_CACHE_HOME}/xmake"
+export XMAKE_PROGRAM_DIR="${XDG_DATA_HOME}/xmake"
+
+# rust
+export RUSTUP_HOME="${XDG_CONFIG_HOME}/rustup"
+export CARGO_HOME="${XDG_CONFIG_HOME}/cargo"
+
+# gradle
+export GRADLE_USER_HOME="${XDG_CONFIG_HOME}/gradle"
+
+# pip
+export PIP_CONFIG_FILE="${XDG_CONFIG_HOME}/pip/pip.conf"
+export PIPX_HOME="${XDG_DATA_HOME}/pipx"
+export PIPX_BIN_DIR="${PIPX_HOME}/bin"
+export PIPX_MAN_DIR="${PIPX_HOME}/man"
+export PIPX_SHARED_LIBS="${PIPX_HOME}/lib"
+export PATH="${PATH}:${PIPX_BIN_DIR}"

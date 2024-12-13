c627f4591402d201d320af75c89ee9b1af25d97e
xiatiandeairen <384660495@qq.com>
2024-12-13 08:37:32 +0800

    feat: add fzf alias and functions
    close #22, closed #20, fixed #52

--- zsh/dot-config/zsh/config/alias.zsh
+++ zsh/dot-config/zsh/config/alias.zsh
@@ -15,7 +15,7 @@ alias el='e -lh --no-user' # mid form -user/group/hidden
 alias et='el --tree -L 3' # mid form tree listing
 
 # fd, -e / --extension: 查找特定扩展名的文件, -p / --path: 指定搜索的路径, -x / --exec: 对每个找到的文件执行命令
-local default_fd_opts="-s -E venv -E .git -E node_module -E '*.log' -E '*.bak'"
+local default_fd_opts="-s -E venv -E .git -E node_modules -E '*.log' -E '*.bak' -E .vscode --strip-cwd-prefix --follow --hidden "
 alias _fd='fd '"${default_fd_opts}"
 alias fdf='_fd -t f'
 alias fdd='_fd -t d'
@@ -23,6 +23,10 @@ alias fdd='_fd -t d'
 # ack 
 local default_ack_opts="-i --context=5 --color"
 alias g='ack '"${default_ack_opts}"
+
+# fzf
+local pattern="\${*:-}"
+alias fzf_vim="fzf --bind 'enter:become(vim {+}),ctrl-e:become(emacs {})'"
 
 # custom
 alias delete-dsstore='find . -name ".DS_Store" -exec rm -f {} \;'

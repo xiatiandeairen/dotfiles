bd897d4eef85761173e980adef5d7371cc51615a
xiatiandeairen <384660495@qq.com>
2024-12-13 22:15:54 +0800

    feat: supplement completion rules

--- zsh/dot-config/zsh/config/completion.zsh
+++ zsh/dot-config/zsh/config/completion.zsh
@@ -33,3 +33,12 @@
 		compinit -C
 	fi
 } ${ZDOTDIR:-$HOME}/.zcompdump 
+
+zstyle ":completion:*" menu no
+zstyle ":completion:*" matcher-list "" "m:{a-zA-Z}={A-Za-z}" "r:|[._-]=* r:|=*" "l:|=* r:|=*"
+zstyle ":completion:*" group-name ""
+zstyle ":completion:*:default" list-colors ${(s.:.)LS_COLORS}
+zstyle ":completion:*:descriptions" format "%F{green}-- %d --%f"
+zstyle ":completion:*:messages" format " %F{purple} -- %d --%f"
+zstyle ":completion:*:warnings" format " %F{red}-- no matches found --%f"
+zstyle ":completion:*:git-checkout:*" sort false

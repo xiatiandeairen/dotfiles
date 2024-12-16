ba27df49e26cae4075b4beef2c14e1264dacc2b0
xiatiandeairen <384660495@qq.com>
2024-12-16 23:42:10 +0800

    feat: support telescope keymaps

---
+++ nvim/dot-config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+local setup, telescope = pcall(require, "telescope")
+if not setup then
+  return
+end
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })

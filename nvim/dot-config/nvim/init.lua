678a9fa1c2ef840af194908bdf9000104e969288
xiatiandeairen <384660495@qq.com>
2024-12-24 00:51:06 +0800

    fix: support keymaps for buffer or window switch
    close #80

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,6 +1,10 @@
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
+vim.cmd('source ./config/keymaps.vim')
+vim.cmd('source ./config/options.vim')
+vim.cmd('source ./config/autocmds.vim')
+
+-- require("config.options")
+-- require("config.keymaps")
+-- require("config.autocmds")
 require("config.lazy")
 
 require("plugins.telescope")

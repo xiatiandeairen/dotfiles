4933fb6f00abe6aa5b27ca53088e44e93099a937
xiatiandeairen <384660495@qq.com>
2024-12-29 23:28:06 +0800

    fix: improve fzf_tmux function to open new session default

--- zsh/dot-config/zsh/scripts/fzf.zsh
+++ zsh/dot-config/zsh/scripts/fzf.zsh
@@ -29,7 +29,7 @@ fzf_tmux() {
   if [ $1 ]; then
     tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
   fi
-  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || tmux
 }
 
 # 仅使用fzf搜索字符串

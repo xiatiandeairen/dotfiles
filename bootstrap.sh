8a503d116f52dcaff3bb1af48c54233b5c9ae17f
xiatiandeairen <384660495@qq.com>
2025-01-13 22:03:59 +0800

    fix: update gradle and maven config

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ function apply {
 }
 
 function bootstrap {
-    targets=(zsh brew bat broot eza tig tmux zoxide vim thefuck nvim direnv kitty emacs)
+    targets=(zsh brew bat broot eza tig tmux zoxide vim thefuck nvim direnv kitty emacs maven gradle)
     for target in "${targets[@]}"; do
         if [ -d ${DOTFILES}/${target} ]; then
             echo "apply ${target} success"

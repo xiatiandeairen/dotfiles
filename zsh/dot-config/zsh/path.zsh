37a1c1680bd031108e7c17c2d1b5178d6ddb4d6f
xiatiandeairen <384660495@qq.com>
2025-01-26 21:04:57 +0800

    fix: add workflow bin

--- zsh/dot-config/zsh/path.zsh
+++ zsh/dot-config/zsh/path.zsh
@@ -1,2 +1,3 @@
 [ -d ${HOME}/.local/bin ] && export PATH="${HOME}/.local/bin:${PATH}"
+[ -d ${DOTFILES}/workflow/bin ] && export PATH="${DOTFILES}/workflow/bin:${PATH}"
 [ -d /opt/homebrew/bin ] && export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}"

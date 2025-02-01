c5469fdb07d14e6e614f3e6b92f8fd0e5b5c2d77
xiatiandeairen <384660495@qq.com>
2025-02-01 16:15:29 +0800

    feat: supplement $HOME/PATH to PATH variable

--- zsh/dot-config/zsh/path.zsh
+++ zsh/dot-config/zsh/path.zsh
@@ -1,3 +1,5 @@
 [ -d ${HOME}/.local/bin ] && export PATH="${HOME}/.local/bin:${PATH}"
 [ -d ${DOTFILES}/workflow/bin ] && export PATH="${DOTFILES}/workflow/bin:${PATH}"
 [ -d /opt/homebrew/bin ] && export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}"
+export PATH="$HOME/.jenv/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
